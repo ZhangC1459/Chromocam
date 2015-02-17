6ddbf3cf5dd76519e6f4612935c699134e7d9714
ZhangC1459 <ZhangC1459@gmail.com>
2015-02-17 16:34:17 -0600

    Fixed all the stupid crap

--- Chromocam/gen/com/chromocam/R.java
+++ Chromocam/gen/com/chromocam/R.java
@@ -24,6 +24,9 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int action_settings=0x7f080000;
+    }
     public static final class layout {
         public static final int activity_events=0x7f030000;
     }
--- Chromocam/src/com/chromocam/Events.java
+++ Chromocam/src/com/chromocam/Events.java
@@ -11,7 +11,6 @@ public class Events extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_events);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
