3654cc3c9969fc6c2ac77b7c00eb02ac1b052365
ZhangC1459 <ZhangC1459@gmail.com>
2015-02-28 19:55:12 -0600

    Added xml layout files for tabs

--- app/src/main/java/com/chromocam/chromocam/LiveStreamTab.java
+++ app/src/main/java/com/chromocam/chromocam/LiveStreamTab.java
@@ -1,9 +1,17 @@
 package com.chromocam.chromocam;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.app.Fragment;
 
-/**
- * Created by ChristZhange on 2/28/2015.
- */
 public class LiveStreamTab extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.livestream, container, false);
+        return rootView;
+    }
+
 }
