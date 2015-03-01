7041b948fdf428b191c1fadf5651ea841eb70afa
ZhangC1459 <ZhangC1459@gmail.com>
2015-02-28 20:30:45 -0600

    put in dummy text on layouts for stream and event lists

--- app/src/main/java/com/chromocam/chromocam/MainActivity.java
+++ app/src/main/java/com/chromocam/chromocam/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
