d28a3502f1fb992f6bcbaf8bd3f4512386fc0abf
ZhangC1459 <ZhangC1459@gmail.com>
2015-04-09 15:30:53 -0500

    Save picture function now works; saves to phone "Pictures" folder

--- app/src/main/java/com/chromocam/chromocam/util/DisplayPictureActivity.java
+++ app/src/main/java/com/chromocam/chromocam/util/DisplayPictureActivity.java
@@ -1,10 +1,12 @@
 package com.chromocam.chromocam.util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -24,6 +26,8 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
@@ -34,6 +38,7 @@ public class DisplayPictureActivity extends Activity {
     private int type = 1;
     private String imageID = "0";
     private Menu menu;
+    private Bitmap image = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +47,7 @@ public class DisplayPictureActivity extends Activity {
         byte[] b = getIntent().getByteArrayExtra("image");
         type = getIntent().getIntExtra("calling", 1);
         imageID = getIntent().getStringExtra("imageID");
-        Bitmap image = BitmapFactory.decodeByteArray(b, 0, b.length);
+        image = BitmapFactory.decodeByteArray(b, 0, b.length);
         ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
         imageView.setImageBitmap(image);
 
@@ -74,6 +79,39 @@ public class DisplayPictureActivity extends Activity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        if (id == R.id.action_save){
+            FileOutputStream out = null;
+            File sd = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsoluteFile(), imageID + ".png");
+                if(!sd.exists()){
+                    try{
+                        sd.createNewFile();
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            try{
+                if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+                    out = new FileOutputStream(sd);
+                    image.compress(Bitmap.CompressFormat.PNG, 100, out);
+                    out.flush();
+                    out.close();
+                    Toast.makeText(getApplicationContext(), "Saved to SD", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "No External Storage detected", Toast.LENGTH_LONG).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         if (id == R.id.action_archive || id == R.id.action_unarchive) {
@@ -125,11 +163,7 @@ public class DisplayPictureActivity extends Activity {
                 //Execute
                 HttpResponse response = httpClient.execute(httpPost);
                 InputStream in = response.getEntity().getContent();
-                if(in!=null){
-                    return "Success!";
-                } else{
-                    return "Failed";
-                }
+
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             } catch (ClientProtocolException e) {
